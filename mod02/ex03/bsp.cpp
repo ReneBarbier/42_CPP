9067251087fd0b8ca9b1dd76b44d74565a1e1528
ReneBarbier <115894509+ReneBarbier@users.noreply.github.com>
2024-04-12 22:51:47 +0200

    bsp working

--- mod02/ex03/bsp.cpp
+++ mod02/ex03/bsp.cpp
@@ -2,24 +2,15 @@
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	Point d(b.getX() - a.getX(), b.getY() - a.getY());
-	Point e(c.getX() - a.getX(), c.getY() - a.getY());
+	Fixed vp = (b.getX() - a.getX()) * (c.getY() - a.getY()) - (b.getY() - a.getY()) * (c.getX() - a.getX());
 	
-	Fixed w1 = (e.getX() * (a.getY() + point.getY()) + e.getY() * (point.getX() - a.getX())) / (d.getX() * e.getY() - d.getY() * e.getX());
-	Fixed w2 = (point.getY() - a.getY() - w1 * d.getY()) / e.getY();
-	
-	std::cout << (point.getY() - a.getY() - w1 * d.getY()) << std::endl;
-	std::cout << "A.x " << a.getX() << " " << "A.y " << a.getY() << std::endl;
-	std::cout << "B.x " << b.getX() << " " << "B.y " << b.getY() << std::endl;
-	std::cout << "C.x " << c.getX() << " " << "C.y " << c.getY() << std::endl;
+	Fixed vp1 = (b.getX() - a.getX()) * (point.getY() - a.getY()) - (b.getY() - a.getY()) * (point.getX() - a.getX());
+	Fixed vp2 = (c.getX() - b.getX()) * (point.getY() - b.getY()) - (c.getY() - b.getY()) * (point.getX() - b.getX());
+	Fixed vp3 = (a.getX() - c.getX()) * (point.getY() - c.getY()) - (a.getY() - c.getY()) * (point.getX() - c.getX());
 
-	std::cout << "P.x " << point.getX() << " " << "P.y " << point.getY() << std::endl;
-
-	std::cout << "E.x " << e.getX() << " " << "E.y " << e.getY() << std::endl;
-	std::cout << "D.x " << d.getX() << " " << "D.y " << d.getY() << std::endl;
-	std::cout << "W1 " << w1.toFloat() << " " << "W2 " << w2.toFloat() << std::endl;
-
-	if (w1 >= 0 && w2 >= 0 && w1 + w2 <= 1)
+	if (vp < 0 && vp1 < 0 && vp2 < 0 && vp3 < 0)
+		return true;
+	if (vp > 0 && vp1 > 0 && vp2 > 0 && vp3 > 0)
 		return true;
 	return false;
 }
--- mod02/ex03/main.cpp
+++ mod02/ex03/main.cpp
@@ -3,12 +3,12 @@
 int main(void) 
 {
 	Point a(1, 1);
-	Point b(1, 2);
-	Point c(2, 1);
+	Point b(3, 3);
+	Point c(3, 1);
 
-	Point p1(1.2, 1.2);
-	Point p2(0.5, 1.5);
-	Point p3(0, 1);
+	Point p1(2, 1.5); //inside 
+	Point p2(3, 3); //on a vertex
+	Point p3(1, 2); //on the edge
 
 	if (bsp(a, b, c, p1))
 		std::cout << "p1 is inside the triangle" << std::endl;
