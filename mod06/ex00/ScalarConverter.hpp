d616d58615705d050af9c0d3c03a168d6a5e86c6
René Barbier Pérez <rbarbier@car9s1.42barcelona.com>
2024-10-24 19:48:48 +0200

    fixing 06

--- mod06/ex00/ScalarConverter.hpp
+++ mod06/ex00/ScalarConverter.hpp
@@ -13,30 +13,10 @@
 class ScalarConverter 
 {
 	private:
-		char	_char;
-		int		_int;
-		float	_float;
-		double	_double;
-
 		ScalarConverter();
 		ScalarConverter(ScalarConverter &cpy);
 		ScalarConverter &operator=(ScalarConverter &cpy);
 		~ScalarConverter();
-
-		void fromChar();
-		void fromInt();
-		void fromFloat();
-		void fromDouble();
-
-		void strToChar(std::string input);
-		void strToInt(std::string input);
-		void strToFloat(std::string input);
-		void strToDouble(std::string input);
-		
-		int getType(std::string input);
-		int checkInput(std::string input);
-		int checkForSpecialCases(std::string input);
-		int checkOverflow(std::string input);
     public:
-		void convert(std::string input);
+		static void convert(std::string input);
 };
--- mod06/ex00/main.cpp
+++ mod06/ex00/main.cpp
@@ -4,7 +4,6 @@ int main(int argc, char **argv)
 {
 	if (argc != 2)
 		return (std::cout << "Error: Invalid number of arguments" << std::endl, 1);
-	ScalarConverter convert;
-	convert.convert(argv[1]);
+	ScalarConverter::convert(argv[1]);
 	return (0);
 }
