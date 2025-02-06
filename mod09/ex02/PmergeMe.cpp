8b8346bbe2353169e2aa3eef53562ef9dac50000
ReneBarbier <115894509+ReneBarbier@users.noreply.github.com>
2025-02-06 22:19:40 +0100

    vec done

--- mod09/ex02/PmergeMe.cpp
+++ mod09/ex02/PmergeMe.cpp
@@ -106,16 +106,6 @@ int jacobsthal(int n)
 	return b;  // Return J(n)
 }
 
-std::list<element>::iterator jacobsthalInit(std::list<element> &pend, int rec_level)
-{
-	std::list<element>::iterator insert = pend.begin();
-	size_t insertions = jacobsthal(rec_level) - jacobsthal(rec_level - 1);
-	if (insertions > pend.size())
-		return std::prev(pend.end());
-	std::advance(insert, insertions - 1);
-	return it;
-}
-
 // initialize the main list
 void initMain(std::list<element> *main, std::vector<int> *vec, int rec_level)
 {
@@ -174,11 +164,17 @@ std::list<element>::iterator binarySearch(std::list<element> &main, int elem, st
 
 	while (it != boundary) {
 		mid = it;
+		std::cout << "Boundary: " << boundary->type << boundary->tag << std::endl;
+		std::cout << "Distance: " << std::distance(it, boundary) / 2 << std::endl;
 		std::advance(mid, std::distance(it, boundary) / 2);
+		std::cout << "Mid: " << mid->type << mid->tag << " " << mid->vec.back() << std::endl;
 		if (elem < mid->vec.back())
 			boundary = mid;
-		else
+		else {
 			it = mid;
+			if (std::distance(it, boundary) == 1)
+				return ++it;
+		}
 	}
 	return it;
 }
@@ -199,19 +195,24 @@ void insertion(std::vector<int> *vec, int rec_level)
 	std::cout << "Odd: " << odd.type << odd.tag << " ";
 	printVec(odd.vec);
 
-	std::list<element>::iterator insert = pend.begin();
 	std::list<element>::iterator tmp_it;
 
+	main.insert(main.begin(), pend.front());
+	pend.pop_front();
+
 	while (!pend.empty()) {
+		std::cout << "Iteration: " << jacob_iteration << std::endl;
 		std::list<element>::iterator insert = pend.begin();
-		int insertions = jacobsthal(jacob_iteration) - jacobsthal(jacob_iteration - 1);
+		size_t insertions = jacobsthal(jacob_iteration) - jacobsthal(jacob_iteration - 1);
 		if (insertions > pend.size())
 			insertions = pend.size();
 		std::advance(insert, insertions - 1);
 		for (; insertions > 0; insertions--) {
 			std::list<element>::iterator boundary = main.begin();
-			while ((*boundary).tag != insert->tag)
+			while (boundary->tag != insert->tag)
 				boundary++;
+			std::cout << "Insert: " << insert->type << insert->tag << " ";
+			std::cout << "Boundary: " << boundary->type << boundary->tag << std::endl;
 	
 			std::list<element>::iterator slot = binarySearch(main, insert->vec.back(), boundary);
 			main.insert(slot, *insert);
@@ -219,28 +220,32 @@ void insertion(std::vector<int> *vec, int rec_level)
 			tmp_it--;
 			pend.erase(insert);
 			insert = tmp_it;
+			std::cout << "Main:\n";
+			printList(main);
+			std::cout << "Pend:\n";
+			printList(pend);
+			//exit(1);
 		}
-	
-		std::list<element>::iterator slot = binarySearch(main, insert->vec.back(), boundary);
-		main.insert(slot, *insert);
-
-		if (insert != pend.begin()) {
-			tmp_it = insert;
-			tmp_it--;
-			pend.erase(insert);
-			insert = tmp_it;
-		}
-		else {
-			pend.erase(insert);
-			insert = jacobsthalInit(pend, jacob_iteration);
-			jacob_iteration++;
-		}
+		jacob_iteration++;
 	}
+
 
 	if (odd.tag != 0) {
 		std::list<element>::iterator slot = binarySearch(main, odd.vec.back(), main.end());
 		main.insert(slot, odd);
+		std::cout << "Main (AFTER ODD):\n";
+		printList(main);
 	}
+
+
+	std::vector<int> new_vec;
+	std::list<element>::iterator it = main.begin();
+	while (it != main.end()) {
+		std::copy(it->vec.begin(), it->vec.end(), std::back_inserter(new_vec));
+		it++;
+	}
+	std::copy(vec->end() - (vec->size() % (1 << rec_level)), vec->end(), std::back_inserter(new_vec));
+	*vec = new_vec;
 
 	if (rec_level == 0)
 		return;
@@ -253,7 +258,7 @@ double PmergeMeVec(std::vector<int> *vec) {
 	time_t start = time(NULL);
 
 	pairSortRec(vec, &rec_level);
-	insertion(vec, rec_level - 1 - 1);
+	insertion(vec, rec_level - 1);
 
 	time_t end = time(NULL);
 	return difftime(end, start);
